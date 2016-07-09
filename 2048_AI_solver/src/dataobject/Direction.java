14b7052c05b6c849c6a7dc911f342934ed94521b
sgmonusg <sagargupta_monu@yahoo.co.in>
2016-07-09 09:35:36 +0530

    added direction code

--- 2048_AI_solver/src/dataobject/Direction.java
+++ 2048_AI_solver/src/dataobject/Direction.java
@@ -1,10 +1,31 @@
 package dataobject;
 
-public class Direction {
 
-	public static Object values() {
-		// TODO Auto-generated method stub
-		return null;
+public enum Direction {
+	UP(0,"Up"),
+	RIGHT(1,"Right"),
+	DOWN(2,"Down"),
+	LEFT(4,"Left");
+	
+	private final int code;
+	private final String description;
+	
+	private Direction(final int code ,final String description){
+		this.code=code;
+		this.description =description;
 	}
+	
+	public int getCode(){
+		return code;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	@Override
+	public String toString(){
+		return description ;
+	}
+	
 
 }
--- 2048_AI_solver/src/game/Board.java
+++ 2048_AI_solver/src/game/Board.java
@@ -1,12 +1,62 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import dataobject.Direction;
 
-public class Board {
+public class Board implements Cloneable {
 
-	public static final Integer BOARD_SIZE = null;
+	public static final Integer BOARD_SIZE = 4;
+	private int[][] boardArray;
+	private final Random randomGenerator;
+	private int cache_emptyCells =(Integer) null;
+	
+	public Board(){
+		boardArray =new int[BOARD_SIZE][BOARD_SIZE];
+		randomGenerator =new Random(System.currentTimeMillis());
+		addRandomCell();
+		addRandomCell();
+	}
 
-	public boolean isGameTerminated() {
+	private void addRandomCell() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object clone() throws CloneNotSupportedException{
+		Board copy= (Board)super.clone();
+		copy.boardArray =clone2dArray(boardArray);
+		return copy;
+		
+	}
+	private int[][] clone2dArray(Object boardArray2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public boolean isGameTerminated() throws CloneNotSupportedException {
+		boolean terminated =false;
+		if(hasWon()==true){
+			terminated =true;
+		}
+		else{
+			if(getNumberOfEmptyCells()==0){
+				Board copyBoard=(Board) this.clone();
+				if(copyBoard.move(Direction.UP)==0&&copyBoard.move(Direction.DOWN)==0&&copyBoard.move(Direction.LEFT)==0&&copyBoard.move(Direction.RIGHT)==0){
+					terminated=true;
+				}
+			}
+		}
+		return terminated;
+		
+		
+		// TODO Auto-generated method stub
+		//return false;
+	}
+
+	public boolean hasWon() {
+		
 		// TODO Auto-generated method stub
 		return false;
 	}
@@ -16,14 +66,19 @@ public class Board {
 		return null;
 	}
 
-	public Object getNumberOfEmptyCells() {
+	public int getNumberOfEmptyCells() {
+		if(cache_emptyCells==(Integer) null){
+			cache_emptyCells=getEmptyCellIds().size();
+		}
+		return cache_emptyCells;
 		// TODO Auto-generated method stub
-		return null;
+		//return null;
 	}
 
-	public Object getBoardArray() {
+	public int[][] getBoardArray() {
 		// TODO Auto-generated method stub
-		return null;
+		return clone2dArray(boardArray);
+		//return null;
 	}
 
 	public int move(Direction direction) {
@@ -37,6 +92,16 @@ public class Board {
 	}
 
 	public java.util.List<Integer> getEmptyCellIds() {
+		List<Integer> cellList =new ArrayList<>();
+		
+		for(int i=0;i<BOARD_SIZE;i++){
+			for(int j=0;j<BOARD_SIZE;j++){
+				if(boardArray[i][j]==0){
+					cellList.add(BOARD_SIZE*i+j);
+				}
+			}
+			
+		}
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -44,6 +109,10 @@ public class Board {
 	public void setEmptyCell(int i, int j, int value) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public Random getRandomGenerator() {
+		return randomGenerator;
 	}
 	
 
